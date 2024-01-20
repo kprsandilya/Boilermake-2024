b21ada622ff8f8042cf224e4f83ca0cf35728ca6
Sharan Suthaharan <143751312+ssuthaharan@users.noreply.github.com>
2024-01-20 11:04:07 -0500

    test

---
+++ Boilermake/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var speed = 50
+var motion = Vector2.ZERO
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.play("sidewalk")
+		$AnimatedSprite2D.flip_h = true
+		motion.x = speed
+		motion.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.play("sidewalk")
+		$AnimatedSprite2D.flip_h = false
+		motion.x = -speed
+		motion.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		$AnimatedSprite2D.play("downwalk")
+		motion.y = speed
+		motion.x = 0
+	elif Input.is_action_pressed("ui_up"): 
+		$AnimatedSprite2D.play("upwalk")
+		motion.y = -speed
+		motion.x = 0
+	else:
+		$AnimatedSprite2D.play("idle")
+		motion.x = 0
+		motion.y = 0
+	velocity = motion
+	move_and_slide()
+
+
