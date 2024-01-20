f7786b4ef601f9004514cfcb75fc6882f678fe82
Sandilya Kambhampati <63815093+kprsandilya@users.noreply.github.com>
2024-01-20 17:03:15 -0500

    Tilling working

--- Boilermake/world.gd
+++ Boilermake/world.gd
@@ -10,7 +10,7 @@ var can_till_custom_data = "can_till"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(background)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,14 +19,15 @@ func _process(delta):
 
 
 func _input(event):
-	player_tile_pos = background.local_to_map($player.position)
+	player_tile_pos = background.local_to_map(background.to_local($player.position))
+		
 	if Input.is_action_just_pressed("till"):
 		var tile_data : TileData = background.get_cell_tile_data(ground_layer, player_tile_pos)
-		print(tile_data)
-		print(player_tile_pos)
-		#print(tile_data.get_custom_data("can_till"))
+		
+		var source_id = 0
+		var atlas_coord = Vector2i(4,7)
 		
 		if tile_data:
 			var can_till = tile_data.get_custom_data(can_till_custom_data)
 			if can_till:
-				background.set_cell(ground_layer, player_tile_pos)
+				background.set_cell(ground_layer, player_tile_pos, source_id, atlas_coord)
