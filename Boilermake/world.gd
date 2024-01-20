d7184c7b4424bf5231c6a374d7decdf325940a5f
Sandilya Kambhampati <63815093+kprsandilya@users.noreply.github.com>
2024-01-20 16:02:10 -0500

    test

--- Boilermake/world.gd
+++ Boilermake/world.gd
@@ -4,23 +4,27 @@ extends Node2D
 
 var ground_layer = 0
 
-var player_tile_pos : Vector2i = background.local_to_map($player.position)
+@onready var player_tile_pos : Vector2i = background.local_to_map($player.position)
 
 var can_till_custom_data = "can_till"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print(background)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	
+func _process(delta):
+	_input(delta)
 
 
 func _input(event):
+	player_tile_pos = background.local_to_map($player.position)
 	if Input.is_action_just_pressed("till"):
 		var tile_data : TileData = background.get_cell_tile_data(ground_layer, player_tile_pos)
+		print(tile_data)
+		print(player_tile_pos)
+		#print(tile_data.get_custom_data("can_till"))
 		
 		if tile_data:
 			var can_till = tile_data.get_custom_data(can_till_custom_data)
