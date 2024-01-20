9050d03887963ab978e70971a4699b3616b837e8
Sharan Suthaharan <143751312+ssuthaharan@users.noreply.github.com>
2024-01-20 15:40:34 -0500

    Attempt to incorporate tiles in code

--- Boilermake/world.gd
+++ Boilermake/world.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+@onready var background = $background
+
+var ground_layer = 0
+
+var player_tile_pos : Vector2i = background.local_to_map($player.position)
+
+var can_till_custom_data = "can_till"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +14,15 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+	
+
+
+func _input(event):
+	if Input.is_action_just_pressed("till"):
+		var tile_data : TileData = background.get_cell_tile_data(ground_layer, player_tile_pos)
+		
+		if tile_data:
+			var can_till = tile_data.get_custom_data(can_till_custom_data)
+			if can_till:
+				background.set_cell(ground_layer, player_tile_pos)
