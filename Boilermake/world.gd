09c4d706f0897025892ebc855789dddfd5303a98
Shivam Patel <143851207+shivpatel222@users.noreply.github.com>
2024-01-20 19:46:49 -0500

    turnip

--- Boilermake/world.gd
+++ Boilermake/world.gd
@@ -9,6 +9,8 @@ var ground_layer = 0
 var can_till_custom_data = "can_till"
 var can_fertilize_custom_data = "can_fertilize"
 var can_seed_corn_custom_data = "can_seed_corn"
+var can_seed_carrot_custom_data = "can_seed_carrot"
+var can_seed_turnip_custom_data = "can_seed_turnip"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -47,7 +49,7 @@ func _input(event):
 			if can_fertilize:
 				background.set_cell(ground_layer, player_tile_pos, source_id, atlas_coord)
 			
-	elif Input.is_action_just_pressed("plant_corn"):
+	if Input.is_action_just_pressed("plant_corn"):
 		tile_data = background.get_cell_tile_data(ground_layer, player_tile_pos)
 		source_id = 0
 		atlas_coord = Vector2i(8, 7)
@@ -55,4 +57,22 @@ func _input(event):
 		if tile_data:
 			var can_seed_corn = tile_data.get_custom_data(can_seed_corn_custom_data)
 			if can_seed_corn:
+				background.set_cell(ground_layer, player_tile_pos, source_id, atlas_coord)
+	if Input.is_action_just_pressed("plant_carrot"):
+		tile_data = background.get_cell_tile_data(ground_layer, player_tile_pos)
+		source_id = 0
+		atlas_coord = Vector2i(9, 8)
+		
+		if tile_data:
+			var can_seed_carrot = tile_data.get_custom_data(can_seed_carrot_custom_data)
+			if can_seed_carrot:
+				background.set_cell(ground_layer, player_tile_pos, source_id, atlas_coord)
+	if Input.is_action_just_pressed("plant_turnip"):
+		tile_data = background.get_cell_tile_data(ground_layer, player_tile_pos)
+		source_id = 0
+		atlas_coord = Vector2i(12, 9)
+		
+		if tile_data:
+			var can_seed_turnip = tile_data.get_custom_data(can_seed_turnip_custom_data)
+			if can_seed_turnip:
 				background.set_cell(ground_layer, player_tile_pos, source_id, atlas_coord)
